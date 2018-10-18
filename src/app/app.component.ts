bb64f88d9c5f4f14e22df9d214c4508750bc696b
Your Name <you@example.com>
2018-10-18 21:28:01 +0300

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form-builder-stud';
+
+  formShema = [
+    { name: 'field1', type: 'string', value: 'Строка' },
+    { name: 'field2', type: 'number', value: 'Номер' },
+    { name: 'field3', type: 'other', value: 'Прочее' },
+  ];
+
+  formGroup: FormGroup = new FormGroup({});
+
+  createControl( ctl ) {
+    this.formGroup.registerControl(ctl.name, new FormControl(ctl.value));
+    return ctl.name;
+  }
+
 }
