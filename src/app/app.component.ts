51bd6e038d46b961327bbd26ec6ae188965bd6c2
Сергей <stvbox@yandex.ru>
2018-10-20 12:01:43 +0300

    nested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'form-builder-stud';
 
   formShema = [
     { name: 'field1', type: 'string', value: 'Строка' },
-    { name: 'field2', type: 'number', value: 'Номер' },
+    { name: 'field2', type: 'number', value: 'Номер', items: [
+      { name: 'field21', type: 'number', value: 'Номер21' },
+      { name: 'field22', type: 'number', value: 'Номер22' }
+    ]},
     { name: 'field3', type: 'other', value: 'Прочее' },
   ];
 
   formGroup: FormGroup = new FormGroup({});
 
-  createControl( ctl ) {
-    this.formGroup.registerControl(ctl.name, new FormControl(ctl.value));
-    return ctl.name;
+  _createControl(field) {
+    const control = new FormControl(field.value);
+    this.formGroup.registerControl(field.name, new FormControl(field.value));
+    return control;
+  }
+
+  createControl( field ) {
+    return this.formGroup.get(field.name) || this._createControl(field);
+  }
+
+  fieldsTrackByFn(index, item) {
+    console.log(index);
+    console.log(item);
+    return index;
   }
 
 }
